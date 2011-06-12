a836c293eb94b1484f0cb796d0e023558c911502
Alex <alex.pszczolkowski@gmail.com>
2011-06-12 23:21:43 +0200

    ajax na głównej

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -10,7 +10,8 @@ class OffersController < ApplicationController
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @offer }
-    end
+      format.js { render :partial => 'offer_body' } 
+    end 
   end
 
   def new
