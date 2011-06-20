09d9f053f955be19f84e42ea0808b5e7d532af2e
Alex <alex.pszczolkowski@gmail.com>
2011-06-20 11:44:59 +0200

    usunieta autentykacja

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
 
-  before_filter :authenticate #if Rails.env.production? 
+  #before_filter :authenticate if Rails.env.production? 
 
   protected 
   def authenticate
