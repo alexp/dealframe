2a2e54db82a52a29b202e1faf0f381bf41f54d2a
Alex <alex.pszczolkowski@gmail.com>
2011-07-18 00:05:36 +0200

    named_scope - bo scope nie dziala na heroku

--- app/models/couppon.rb
+++ app/models/couppon.rb
@@ -3,9 +3,9 @@ class Couppon < ActiveRecord::Base
   belongs_to :user
   belongs_to :offer
   
-  scope :payed, 
+  named_scope :payed, 
     :conditions => { :status => 1 }
-  scope :payment_pending,
+  named_scope :payment_pending,
     :conditions => { :status => 0  }
 
   def generate_security_code(length)
