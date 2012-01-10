b1e3391e17f8fa96db12e8d24533c8c2e5c2da4a
Alex <alex.pszczolkowski@gmail.com>
2012-01-10 16:06:07 +0100

    pdfy

--- app/controllers/couppons_controller.rb
+++ app/controllers/couppons_controller.rb
@@ -19,8 +19,7 @@ class CoupponsController < ApplicationController
           respond_to do |format|
             format.html
             format.pdf do
-              render :pdf => "test.pdf",
-                     :template => 'couppons/show.pdf.haml',
+              render :template => 'couppons/show.pdf.haml',
                      :orientation => 'landscape',
                      :encoding => 'utf-8'
             end
